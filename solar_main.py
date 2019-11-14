0d7ca8a0d3d0574f6766cce3d4aa833bab264666
Mitya Tsarev <mitya.tsarev@gmail.com>
2019-11-14 22:58:53 +0300

    Minor window size changes

--- solar_main.py
+++ solar_main.py
@@ -120,7 +120,7 @@ def main():
     space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
     space.pack(side=tkinter.TOP)
     # нижняя панель с кнопками
-    frame = tkinter.Frame(space)  # was from root
+    frame = tkinter.Frame(root)
     frame.pack(side=tkinter.BOTTOM)
 
     start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
